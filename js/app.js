1bc262506eda5cf88d2792d282101d4e40f2b751
shaolin <shaolinruyi@hotmail.com>
2018-03-07 15:29:15 +0800

    function complete

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
 /*
  * Create a list that holds all of your cards
  */
-
+ let card = document.getElementsByClassName("card");
+ let cards = [...card];
 
 /*
  * Display the cards on the page
@@ -9,6 +10,45 @@
  *   - loop through each card and create its HTML
  *   - add each card's HTML to the page
  */
+ const deck = document.getElementsByClassName("deck");
+ let moves = 0;
+ let counter = document.querySelector(".moves");
+ const stars = document.querySelectorAll(".fa-star");
+ let matchedCard = document.getElementsByClassName("match");
+ let starsList = document.querySelectorAll(".stars li");
+ let closeicon = document.querySelector(".close");
+ let modal = document.getElementById("popup1");
+ let openedCards = [];
+ let second = 0, minute = 0; hour = 0;
+ let timer = document.querySelector(".timer");
+ let interval;
+
+ document.body.onload = startGame();
+
+ function startGame(){
+     cards = shuffle(cards);
+     for (var i = 0; i < cards.length; i++){
+         deck.innerHTML = "";
+         [].forEach(function(item) {
+             deck.appendChild(item);
+         });
+         cards[i].classList.remove("show", "open", "match", "disabled");
+     };
+
+    moves = 0;
+    counter.innerHTML = moves;
+
+    for (var i= 0; i < stars.length; i++){
+        stars[i].style.color = "#FFD700";
+        stars[i].style.visibility = "visible";
+    };
+
+    second = 0;
+    minute = 0;
+    hour = 0;
+    timer.innerHTML = "0 mins 0 secs";
+    clearInterval(interval);
+};
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
@@ -23,7 +63,7 @@ function shuffle(array) {
     }
 
     return array;
-}
+};
 
 
 /*
@@ -36,3 +76,141 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+ for (var i = 0; i < cards.length; i++){
+     card = cards[i];
+     card.addEventListener("click", displayCard);
+     card.addEventListener("click", cardOpen);
+     card.addEventListener("click",congratulations);
+ };
+
+function displayCard(){
+   this.classList.toggle("open");
+   this.classList.toggle("show");
+   this.classList.toggle("disabled");
+};
+
+function cardOpen() {
+    openedCards.push(this);
+    let len = openedCards.length;
+    if(len === 2){
+        moveCounter();
+        if(openedCards[0].type === openedCards[1].type){
+            matched();
+        } else {
+            unmatched();
+        }
+    }
+};
+
+function matched(){
+    openedCards[0].classList.add("match", "disabled");
+    openedCards[1].classList.add("match", "disabled");
+    openedCards[0].classList.remove("show", "open");
+    openedCards[1].classList.remove("show", "open");
+    openedCards = [];
+};
+
+function unmatched(){
+    openedCards[0].classList.add("unmatched");
+    openedCards[1].classList.add("unmatched");
+    disable();
+    setTimeout(function(){
+        openedCards[0].classList.remove("show", "open", "unmatched");
+        openedCards[1].classList.remove("show", "open", "unmatched");
+        enable();
+        openedCards = [];
+    },800);
+};
+
+function disable(){
+    Array.prototype.filter.call(cards, function(card){
+        card.classList.add('disabled');
+    });
+}
+
+function enable(){
+    Array.prototype.filter.call(cards, function(card){
+        card.classList.remove('disabled');
+        for(var i = 0; i < matchedCard.length; i++){
+            matchedCard[i].classList.add("disabled");
+        }
+    });
+}
+
+function moveCounter(){
+    moves++;
+    counter.innerHTML = moves;
+    //start timer on first click
+    if(moves == 1){
+        second = 0;
+        minute = 0;
+        hour = 0;
+        startTimer();
+    }
+    // setting rates based on moves
+    if (moves > 12 && moves < 18){
+        for( i= 0; i < 3; i++){
+            if(i > 1){
+                stars[i].style.visibility = "collapse";
+            }
+        }
+    }
+    else if (moves >= 18){
+        for( i= 0; i < 3; i++){
+            if(i > 0){
+                stars[i].style.visibility = "collapse";
+            }
+        }
+    }
+}
+
+// description game timer
+function startTimer(){
+    interval = setInterval(function(){
+        timer.innerHTML = minute+"mins "+second+"secs";
+        second++;
+        if(second == 60){
+            minute++;
+            second=0;
+        }
+        if(minute == 60){
+            hour++;
+            minute = 0;
+        }
+    },1000);
+}
+
+// description congratulations when all cards match, show modal and moves, time and rating
+function congratulations(){
+    if (matchedCard.length == 16){
+        clearInterval(interval);
+        finalTime = timer.innerHTML;
+
+        // show congratulations modal
+        modal.classList.add("show");
+
+        // declare star rating variable
+        let starRating = document.querySelector(".stars").innerHTML;
+
+        //showing move, rating, time on modal
+        document.getElementById("finalMove").innerHTML = moves;
+        document.getElementById("starRating").innerHTML = starRating;
+        document.getElementById("totalTime").innerHTML = finalTime;
+
+        //closeicon on modal
+        closeModal();
+    };
+}
+
+// description close icon on modal
+function closeModal(){
+    closeicon.addEventListener("click", function(e){
+        modal.classList.remove("show");
+        startGame();
+    });
+}
+
+function playAgain(){
+    modal.classList.remove("show");
+    startGame();
+}
